3b28405f495ae07690e1c5d852372b9a4a778d14
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-09-20 19:11:41 +0300

    finish working on Cart system

--- frontend/src/app/components/pages/cart-page/cart-page.component.ts
+++ frontend/src/app/components/pages/cart-page/cart-page.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { Cart } from 'src/app/shared/models/Cart';
+import { CartItem } from 'src/app/shared/models/CartItem';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
-  styleUrls: ['./cart-page.component.css']
+  styleUrls: ['./cart-page.component.css'],
 })
 export class CartPageComponent {
+  cart!: Cart;
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.getCartObservable().subscribe((cart) => {
+      this.cart = cart;
+    });
+  }
+
+  removeFromCart(cartItem: CartItem): void {
+    this.cartService.removeFromCart(cartItem.food.id);
+  }
+
+  changeQuantity(cartitem: CartItem, quantityInString: string): void {
+    const quantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartitem.food.id, quantity);
+  }
 }
