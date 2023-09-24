30f3b1820a34d0a1c86ce521138307a78fcf24c1
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-09-24 22:32:56 +0300

    backend code refactor and cleaning

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,8 @@
 import cors from "cors";
 import express from "express";
-import jwt from "jsonwebtoken";
-import { sample_foods, sample_tags, sample_users } from "../Data";
+
+import foodRouter from "./router/food.router";
+import userRouter from "./router/user.router";
 
 const app = express();
 app.use(express.json());
@@ -12,59 +13,8 @@ app.use(
   })
 );
 
-app.get("/api/foods", (req, res) => {
-  res.send(sample_foods);
-});
-
-app.get("/api/foods/search/:searchTerm", (req, res) => {
-  const searchTerm = req.params.searchTerm;
-  const foods = sample_foods.filter((food) => food.name.toLowerCase().includes(searchTerm.toLowerCase()));
-  res.send(foods);
-});
-
-app.get("/api/foods/tags", (req, res) => {
-  res.send(sample_tags);
-});
-
-app.get("/api/foods/tags/:tagName", (req, res) => {
-  const tagName = req.params.tagName;
-  const foods = sample_foods.filter((food) => food.tags?.includes(tagName));
-  res.send(foods);
-});
-
-app.get("/api/foods/:foodId", (req, res) => {
-  const foodId = req.params.foodId;
-  const food = sample_foods.find((food) => food.id === foodId);
-  res.send(food);
-});
-
-app.post("/api/users/login", (req, res) => {
-  const { email, password } = req.body;
-  const user = sample_users.find((user) => user.email === email && user.password === password);
-
-  if (user) {
-    res.send(generateTokenResponse(user));
-  } else {
-    const BAD_REQUEST = 400;
-    res.status(BAD_REQUEST).send("Username or password is invalid!");
-  }
-});
-
-const generateTokenResponse = (user: any) => {
-  const token = jwt.sign(
-    {
-      email: user.email,
-      isAdmin: user.isAdmin,
-    },
-    "SomeRandomText",
-    {
-      expiresIn: "30d",
-    }
-  );
-
-  user.token = token;
-  return user;
-};
+app.use("/api/foods", foodRouter);
+app.use("/api/user", userRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
