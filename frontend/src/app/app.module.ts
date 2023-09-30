837a1a055ee4672ed0934ad5b5154785cfcfa7da
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-09-30 15:56:09 +0300

    remove removed component from ngModel

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { CheckoutPageComponent } from './components/pages/checkout-page/checkout
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { OrderTackPageComponent } from './components/pages/order-tack-page/order-tack-page.component';
+import { PaymentPageComponent } from './components/pages/payment-page/payment-page.component';
 import { RegisterPgeComponent } from './components/pages/register-pge/register-pge.component';
 import { DefaultButtonComponent } from './components/partials/default-button/default-button.component';
 import { HeaderComponent } from './components/partials/header/header.component';
@@ -22,16 +24,13 @@ import { LoadingComponent } from './components/partials/loading/loading.componen
 import { MapComponent } from './components/partials/map/map.component';
 import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 import { OrderItemsListComponent } from './components/partials/order-items-list/order-items-list.component';
+import { PaypalButtonComponent } from './components/partials/paypal-button/paypal-button.component';
 import { SearchComponent } from './components/partials/search/search.component';
 import { StarRatingComponent } from './components/partials/star-rating/star-rating.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { TextInputComponent } from './components/partials/text-input/text-input.component';
 import { TitleComponent } from './components/partials/title/title.component';
 import { LoadingInterceptor } from './shared/interceptors/loading.interceptor';
-import { PaymentPageComponent } from './components/pages/payment-page/payment-page.component';
-import { PaypalButtonComponent } from './components/partials/paypal-button/paypal-button.component';
-import { OtderTackPageComponent } from './components/pages/otder-tack-page/otder-tack-page.component';
-import { OrderTackPageComponent } from './components/pages/order-tack-page/order-tack-page.component';
 
 @NgModule({
   declarations: [
@@ -57,7 +56,6 @@ import { OrderTackPageComponent } from './components/pages/order-tack-page/order
     MapComponent,
     PaymentPageComponent,
     PaypalButtonComponent,
-    OtderTackPageComponent,
     OrderTackPageComponent,
   ],
   imports: [
